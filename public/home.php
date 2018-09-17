7c30bcb0bd34ae70a1f11e6960e55ea8ec9c67c2
msarevalo <msarevalo@unbosque.edu.co>
2018-09-17 12:58:12 -0500

    cambios en home

--- public/home.php
+++ public/home.php
@@ -22,10 +22,9 @@ if (!isset($_SESSION['username'])){
         <ul>
             <li><a href="#"><i class="fa fa-home"></i><?php echo $_SESSION['username']?></a></li>
             <li><a href="" class="active"><i class="fa fa-home"></i>Inicio</a></li>
-            <li><a href=""><i class="fa fa-laptop"></i>Personajes</a></li>
-            <li><a href=""><i class="fa fa-laptop"></i>Usuarios</a></li>
-            <li><a href=""><i class="fa fa-laptop"></i>Asignacion de Personajes</a></li>
-            <li><a href=""><i class="fa fa-laptop"></i>Aleatorio</a></li>
+            <li><a href=""><i class="fa fa-laptop"></i>Muro</a></li>
+            <li><a href=""><i class="fa fa-laptop"></i>Buzon</a></li>
+            <li><input type="text" placeholder="Prueba"><a href=""><i class="fa fa-laptop"></i><button>Enviar</button></a></li>
             <li><a href="../back/cerrar.php"><i class="fa fa-phone"></i>Salir<img src="../img/cerrar.png" style="width: 15px"></a></li>
         </ul>
     </nav></div>
