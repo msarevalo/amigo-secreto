3458df898eb4ef7e7cab452336ffedec4390c2d9
msarevalo <msarevalo@unbosque.edu.co>
2018-09-17 12:22:47 -0500

    cambios en nueva contaseña

--- public/nueva-pass.php
+++ public/nueva-pass.php
@@ -1,1 +1,23 @@
-<label>Entro ok</label>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Celmedia | Amigo Secreto</title>
+    <meta charset="UTF-8">
+    <!-- Estilos -->
+    <link href="../css/estilos.css" rel="stylesheet">
+    <!-- Scripts -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="../js/index.js" type="application/javascript"></script>
+</head>
+<body onload="noVolver()">
+<div>
+    <form>
+        <label for="pass">Contraseña</label>
+        <input type="password" placeholder="Contraseña" id="pass" name="pass" required><br><br>
+        <label for="rpass">Repetir Contraseña</label>
+        <input type="password" placeholder="Contraseña" id="rpass" name="rpass" required><br><br>
+        <input type="submit" value="Reestablecer">
+    </form>
+</div>
+</body>
+</html>
