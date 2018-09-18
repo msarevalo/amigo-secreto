dcc387629a28158f6e2c19807329acb8529cd408
msarevalo <msarevalo@unbosque.edu.co>
2018-09-18 11:26:32 -0500

    conexion ok

--- back/conexion.php
+++ back/conexion.php
@@ -3,7 +3,7 @@
 session_start();
 
 //$con = mysqli_connect("localhost", "celmedia","ninguna123.", "asecreto");
-//$con = mysqli_connect("localhost", "root", "celmedia_2017", "asecreto");
+$con = mysqli_connect("localhost", "root", "celmedia_2017", "asecreto");
 //$con = mysqli_connect("localhost", "celmedia", "ninguna123.", "asecreto");
 
 if (!$con){
