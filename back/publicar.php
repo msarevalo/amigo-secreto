ce90a096e001ea9e2dc5d2bb3eaf30aa25467729
msarevalo <msarevalo@unbosque.edu.co>
2018-09-18 18:38:45 -0500

    nueva hora

--- back/publicar.php
+++ back/publicar.php
@@ -14,7 +14,7 @@ if ($usuario != 0 ){
     $usuario = $_SESSION['id'];
 }
 
-date_default_timezone_set('America/Bogota'); // your user's timezone
+date_default_timezone_set('UTC -5'); // your user's timezone
 $my_datetime='2013-10-23 15:47:10';
 $dia = date('Y-m-d H:i:s',strtotime("$my_datetime UTC"));
 
