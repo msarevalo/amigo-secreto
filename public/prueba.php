b79a7ae6fe05c93e8e7acab6751bf8972ebe9f9e
msarevalo <msarevalo@unbosque.edu.co>
2018-09-19 17:09:56 -0500

    prueba

--- public/prueba.php
+++ public/prueba.php
@@ -1,44 +1,63 @@
 <?php
-session_start();
-//$con = mysqli_connect("localhost", "manuel","scout950911", "cuentas");
-//if ($con){
-//$resultado = mysqli_query($con, "SELECT * FROM `users` WHERE `user` LIKE '" . $_SESSION['user_correo'] . "'");
-//$respuesta = mysqli_fetch_all($resultado);
-//if ($respuesta){
-//$to = $respuesta[0][6];
-$to = "msscout11@gmail.com";
-$subject = "Reestablecer Contraseña";
-//$usuario = $respuesta[0][1];
-$usuario = "msarevalo";
-$nombre = "Manuel";
-$pass = "Prueba";
-//}
-//}
-//$to = 'msscout11@gmail.com'; // note the comma
-// Subject
-//$subject = 'Pruebas';
-// Message
-$message = '
-<html>
-    <head>
-        <title>Reestablecer Contraseña</title>
-    </head>
-    <body>
-    <label>Haga click en el siguiente link para reestablecer la contraseña</label>
-    <a href="http://localhost:63342/Cuentas/inicio/validar.php?usuario=' . $usuario . '&pass=' . $pass . '">Cambiar mi contraseña</a>
-</body>
-</html>
-';
-// To send HTML mail, the Content-type header must be set
-$headers[] = 'MIME-Version: 1.0';
-$headers[] = 'Content-type: text/html; charset=iso-8859-1';
-// Additional headers
-$headers[] = 'To: ' . $nombre . ' <' . $to . '>';
-$headers[] = 'From: Soporte Cuentas <soporte@cuentas.com>';
-// Mail it
-if (mail($to, $subject, $message, "From: soporte@cuentas.com" . implode("\r\n", $headers))){
-    echo "<script>alert('Mensaje de reestablecer contraseña enviado'); window.location.href=''</script>";
-}else{
-    echo "<script>alert('Algo ha pasado')</script>";
+
+require '../libs/PHPMailer/class.phpmailer.php';
+require '../libs/PHPMailer/class.smtp.php';
+
+$mail = new PHPMailer;
+
+//Tell PHPMailer to use SMTP
+$mail->isSendMail(); //or $mail->isSMTP();
+
+//Enable SMTP debugging
+// 0 = off (for production use)
+// 1 = client messages
+// 2 = client and server messages
+$mail->SMTPDebug = 2;
+
+$mail->Timeout=60;
+
+$mail->Helo = "dominio.com"; //Muy importante para que llegue a hotmail y otros
+
+//Set the hostname of the mail server
+$mail->Host = 'smtp.gmail.com';
+
+//Set the SMTP port number - 587 for authenticated TLS, a.k.a. RFC4409 SMTP submission
+$mail->Port = 587;
+
+//Set the encryption system to use - ssl (deprecated) or tls
+$mail->SMTPSecure = 'tls';
+
+//Whether to use SMTP authentication
+$mail->SMTPAuth = true;
+
+//Username to use for SMTP authentication - use full email address for gmail
+$mail->Username = "amormania.celmedia@gmail.com";
+
+//Password to use for SMTP authentication
+$mail->Password = "euuhsmfyahxymdox";
+
+//Set who the message is to be sent from
+$mail->setFrom('amormania.celmedia@gmail.com', 'Amormania');
+
+//Set an alternative reply-to address
+$mail->addReplyTo('correo@envia.com', 'Nombre');
+
+//Set who the message is to be sent to
+$mail->addAddress('soportecolombia@celmedia.com');
+
+//Set the subject line
+$mail->Subject = 'PHPMailer GMail SMTP test';
+
+//$mail->msgHTML(file_get_contents('contents.html'), dirname(__FILE__));
+
+$mail->Body = '<h1>This is a plain-text message body</h1> Lorem ipsum dolor sit amet, consectetur.';
+
+$mail->IsHTML(true);
+
+//send the message, check for errors
+if (!$mail->send()) {
+    echo "Mailer Error: " . $mail->ErrorInfo;
+} else {
+    echo "Message sent!";
 }
-?>
+
