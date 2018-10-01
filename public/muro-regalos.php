f132b856bc12f2e94390b533d4c99b30bc6011f7
msarevalo <msarevalo@unbosque.edu.co>
2018-10-01 18:06:54 -0500

    cambios

--- public/muro-regalos.php
+++ public/muro-regalos.php
@@ -55,8 +55,8 @@ $resas = mysqli_fetch_all($asignacion);
     if ($resregalo){
         echo "
     <header id='public'>Edtia tu regalo</header>
-    <form method=\"post\" action=\"../back/regaloedit.php\" id=\"publicar\">
-        <textarea required maxlength=\"600\" rows=\"5\" cols=\"85\" placeholder=\"¿Quieres un viaje, un carro, una casa? ¡Escribe aquí qué es lo que quieres!\" id=\"edicion\" name=\"publicacion\" onpaste=\"contarcaracteres();\" onkeyup=\"contarcaracteres2();\">" . $resregalo[0][2] . "</textarea><br>
+    <form method=\"post\" action=\"../back/regaloedit.php\" id=\"publicar\" name='publicar'>
+        <textarea required maxlength=\"600\" rows=\"5\" cols=\"85\" placeholder=\"¿Quieres un viaje, un carro, una casa? ¡Escribe aquí qué es lo que quieres!\" id=\"publicacion\" name=\"publicacion\" onpaste=\"contarcaracteres();\" onkeyup=\"contarcaracteres2();\">" . $resregalo[0][2] . "</textarea><br>
         <img src=\"../img/emoji.png\" style=\"width: 20px; margin-left: 79%; cursor: pointer\" id=\"notificationLink\">
         <label id=\"res\" style=\"color: #bbbbbb;\">0 / 600</label><br>
         <button style=\"margin-left: 80%\" id=\"boton\">Guardar</button><br><br>
@@ -64,7 +64,7 @@ $resas = mysqli_fetch_all($asignacion);
     }else{
         echo "
     <header id='public'>Cuéntale al mundo que quieres de regalo</header>
-    <form method=\"post\" action=\"../back/regalo.php\" id=\"publicar\">
+    <form method=\"post\" action=\"../back/regalo.php\" id=\"publicar\" name='publicar'>
         <textarea required maxlength=\"600\" rows=\"5\" cols=\"85\" placeholder=\"¿Quieres un viaje, un carro, una casa? ¡Escribe aquí qué es lo que quieres!\" id=\"publicacion\" name=\"publicacion\" onpaste=\"contarcaracteres();\" onkeyup=\"contarcaracteres();\"></textarea><br>
         <img src=\"../img/emoji.png\" style=\"width: 20px; margin-left: 79%; cursor: pointer\" id=\"notificationLink\">
         <label id=\"res\" style=\"color: #bbbbbb;\">0 / 600</label><br>";
