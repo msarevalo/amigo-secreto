25824b483d36f611f83ce1a229b58ecfe936f95a
msarevalo <msarevalo@unbosque.edu.co>
2018-10-04 16:32:44 -0500

    textarea

--- public/muro-regalos.php
+++ public/muro-regalos.php
@@ -56,7 +56,7 @@ $resas = mysqli_fetch_all($asignacion);
         echo "
     <header id='public'>Edtia tu regalo</header>
     <form method=\"post\" action=\"../back/regaloedit.php\" id=\"publicar\" name='publicar'>
-        <textarea required maxlength=\"600\" rows=\"5\" cols=\"85\" placeholder=\"¿Quieres un viaje, un carro, una casa? ¡Escribe aquí qué es lo que quieres!\" id=\"publicacion\" name=\"publicacion\" onpaste=\"contarcaracteres2();\" onkeyup=\"contarcaracteres2();\">" . $resregalo[0][2] . "</textarea><br>
+        <textarea required maxlength=\"600\" rows=\"5\" cols=\"85\" placeholder=\"¿Quieres un viaje, un carro, una casa? ¡Escribe aquí qué es lo que quieres!\" id=\"publicacion\" name=\"publicacion\" onpaste=\"contarcaracteres();\" onkeyup=\"contarcaracteres();\">" . $resregalo[0][2] . "</textarea><br>
         <img src=\"../img/emoji.png\" style=\"width: 20px; margin-left: 79%; cursor: pointer\" id=\"notificationLink\">
         <label id=\"res\" style=\"color: #bbbbbb;\">0 / 600</label><br>
         <button style=\"margin-left: 80%\" id=\"boton\">Guardar</button><br><br>
@@ -65,7 +65,7 @@ $resas = mysqli_fetch_all($asignacion);
         echo "
     <header id='public'>Cuéntale al mundo que quieres de regalo</header>
     <form method=\"post\" action=\"../back/regalo.php\" id=\"publicar\" name='publicar'>
-        <textarea required maxlength=\"600\" rows=\"5\" cols=\"85\" placeholder=\"¿Quieres un viaje, un carro, una casa? ¡Escribe aquí qué es lo que quieres!\" id=\"publicacion\" name=\"publicacion\" onpaste=\"contarcaracteres2();\" onkeyup=\"contarcaracteres2();\"></textarea><br>
+        <textarea required maxlength=\"600\" rows=\"5\" cols=\"85\" placeholder=\"¿Quieres un viaje, un carro, una casa? ¡Escribe aquí qué es lo que quieres!\" id=\"publicacion\" name=\"publicacion\" onpaste=\"contarcaracteres();\" onkeyup=\"contarcaracteres();\"></textarea><br>
         <img src=\"../img/emoji.png\" style=\"width: 20px; margin-left: 79%; cursor: pointer\" id=\"notificationLink\">
         <label id=\"res\" style=\"color: #bbbbbb;\">0 / 600</label><br>";
         if (sizeof($resas) != 0) {
